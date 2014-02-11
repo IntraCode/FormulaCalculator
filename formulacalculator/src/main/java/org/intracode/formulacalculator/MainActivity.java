d81815d3b732f5937548464779c4dc4403d15d4c
Johnny Manson <contact@intracode.org>
2014-02-12 00:00:33 +0100

    Initial commit

--- formulacalculator/src/main/java/org/intracode/formulacalculator/MainActivity.java
+++ formulacalculator/src/main/java/org/intracode/formulacalculator/MainActivity.java
@@ -1,15 +1,18 @@
 package org.intracode.formulacalculator;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -18,7 +21,40 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ListView lvFormulas = (ListView) findViewById(R.id.lvFormulas); //initialize the listview
+        Button btnChooseFormula = (Button) findViewById(R.id.btnChooseFormula);
+        String[] formulas = new String[] { // string-array with the supported formulas
+                "PQ-Formula",
+                "ABC-Formula",
+                "URI-Formulas",
+                "Pythagorean Theorem",
+                "Linear Functions",
+                "Exponential Functions"
+        };
+        //creating an adapter for the listview
+        ArrayAdapter<String> lvFormulasAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_single_choice, formulas) {
 
+            //using method to change the text color (white > black)
+            @Override
+            public View getView (int position, View convertView, ViewGroup parent) {
+                View mView = super.getView(position, convertView, parent);
+                if (mView != null) {
+                TextView textView = (TextView) mView.findViewById(android.R.id.text1);
+                textView.setTextColor(Color.BLACK);
+                }
+                return mView;
+            }
+        };
+
+        lvFormulas.setAdapter(lvFormulasAdapter); //set the adapter
+
+        btnChooseFormula.setOnClickListener(new View.OnClickListener() { //setting the click listener for the button
+            @Override
+            public void onClick(View v) {
+                //TODO: pass through parameters to the next activity
+                //TODO: create an enum with all possibilities
+            }
+        });
     }
 
 
@@ -29,15 +65,28 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
+    AlertDialog alert; //alert dialog for later reference; placed within the class to avoid final declaration
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_about) { //check which button is clicked
+
+            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this); //creating the alert builder
+            alertBuilder.setIcon(android.R.drawable.ic_menu_info_details) //setting the icon
+            .setMessage("Developed By:\nJohnny Manson & Ben Nathan") //setting the message
+            .setTitle("App Information") //Setting the Message
+            .setNeutralButton("Got it!", new DialogInterface.OnClickListener() { //setting the neutral button's click listener
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    if (alert != null) //check if alert has been assigned
+                        alert.dismiss(); //get rid of the dialog popup
+                }
+            });
+            alert = alertBuilder.create(); //assign the alert by using the create method
+            alert.show(); //display the alert
         }
         return super.onOptionsItemSelected(item);
     }
